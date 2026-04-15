d017fdc55c6a43cd6dba518c2a78a131b3f569ad
Pooja-S-2006 <poojasenthil@1521>
2026-04-15 20:14:26 +0530

    Used submodule function

--- main-project/app.py
+++ main-project/app.py
@@ -1,1 +1,8 @@
-print("Main Project Started")
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'libs/calculator-lib'))
+
+from calc import add
+
+print("Addition:", add(5, 3))
